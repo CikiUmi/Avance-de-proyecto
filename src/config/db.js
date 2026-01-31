94628cd14aeb207e47273e78d150b7a89d1c5254
Dhh <al02989955@tecmilenio.mx>
2026-01-31 01:05:01 -0600

    Conexión a mongoDB, archivos placeholder

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+/* Conexión a MongoDB usando variable de .env */
+
+const mongoose = require('mongoose');
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("¡Conexión exitosa a MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+/* Fragmento de código extraído de MongoAtlas */
