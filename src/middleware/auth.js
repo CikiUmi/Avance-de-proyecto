9453d0d45e5167a6fb666601929cb8cdc6457e67
Dhh <al02989955@tecmilenio.mx>
2026-02-06 07:13:14 -0600

    nav bar ig

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,6 +10,8 @@ const auth = (req, res, next) => {
     // payload puede contener { id: ..., iat: ..., exp: ... }
     req.user = { id: payload.id };
     next();
+    //const userRole = decoded.role;
+    //console.log('User role:', userRole);
   } catch (err) {
     return res.status(401).json({ message: 'El Token es inválido o ya expiró.' });
   }
